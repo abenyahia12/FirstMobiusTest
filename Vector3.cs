9465f7719312965b45258d6ea7c6fe8ecfaa0062
abdelazizBenYahia <abenyahia@wpi.edu>
2022-02-13 20:03:49 +0100

    Part 4
    in this part , I changed some of the namings and comments then added the normalize and magnitude functions

--- Vector3.cs
+++ Vector3.cs
@@ -16,15 +16,15 @@ public struct Vector3
     }
     //some usefull shortcuts inspired from previous experience
     // this returns a new vector 3
-    public static Vector3 zero
+    public static Vector3 Zero
     {
         get
         {
             return new Vector3(0, 0, 0);
         }
     }
-    // this returns the identity value of Vector3(1 1 1)
-    public static Vector3 one
+    // this returns the identity value of Vector3(1,1,1)
+    public static Vector3 One
     {
         get
         {
@@ -33,24 +33,35 @@ public struct Vector3
     }
     ///////////////////////////////////////////////////////////////////////////////////
     //Methods 
-    //+ operator 
+    // "+" operator 
     public static Vector3 operator +(Vector3 a, Vector3 b)
     {
         return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
     }
-    //- operator
+    // "-" operator
     public static Vector3 operator +(Vector3 a, Vector3 b)
     {
         return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
     }
-    //* operator
+    // "*" operator
     public static Vector3 operator *(Vector3 a, float multiplier)
     {
         return new Vector3(multiplier * a.X, multiplier * a.Y, multiplier * a.Z);
     }
-    /// operator
+    // "/" operator
     public static Vector3 operator /(Vector3 a, float divider)
     {
         return new Vector3(a.X / divider, a.Y / divider, a.Z / divider);
     }
+    //   ||V||=sqrt(x²+y²+z²) this returns the magnitude of a vector a.
+    public float GetMagnitude(Vector3 a)
+    {
+        return Math.Sqrt(Math.Pow(a.X, 2) + Math.Pow(a.Y, 2) + Math.Pow(a.Z, 2));
+    }
+    //     normal = a/ ||a|| which translates to a divided by magnitude function.
+    public Vector3 GetNormalized(Vector3 a)
+    {
+        return a / GetMagnitude(a);
+    }
+
 }
